47be877acfa93fe3536fd562ab159d8837acce49
Arti2911 <139011882+Arti2911@users.noreply.github.com>
2025-01-20 15:51:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+let form=document.getElementById("form")
+form.addEventListener("submit",submitHandler)
+function submitHandler(event){
+	event.preventDefault()
+    let nameInput=document.getElementById("name").value;
+    let ageInput=document.getElementById("age").value;
+    // console.log(event)
+    if(!nameInput || !ageInput){
+        alert("Please fill in all fields")
+        return ;
+    }
+
+    let age=Number(ageInput)
+    const ageCheckPromise=new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+	        if(age>18)resolve(`Welcome, ${nameInput}. You can vote.`)
+	        else reject(`Oh sorry ${nameInput}. You aren't old enough.`)
+        },4000)
+    })
+    ageCheckPromise.then((message)=>alert(messag)).catch((error)=>alert(error))
+            
+}
